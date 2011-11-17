c27dc15ce0931c3fdc971ed0c27b1eca295e9859
John Ruddell <John@new-host.home>
2011-11-16 22:28:34 -0600

    Initial Commit

---
+++ Test/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Test
+//
+//  Created by John Ruddell on 11/16/11.
+//  Copyright (c) 2011 Collin College. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
